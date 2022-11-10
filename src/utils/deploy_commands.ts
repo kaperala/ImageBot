c4f36e07ee3b784bdf1bc8648638fbb2b430370a
Kalle Perälöä <peralakalle1@gmail.com>
2022-11-10 18:21:45 +0200

    fix: fixed deploy_commands for global deployment

--- src/utils/deploy_commands.ts
+++ src/utils/deploy_commands.ts
@@ -1,4 +1,4 @@
-import { REST, Routes } from 'discord.js';
+import { REST, Routes, SlashCommandBuilder } from 'discord.js';
 import dotenv from 'dotenv';
 import * as fs from 'node:fs';
 
@@ -12,7 +12,7 @@ const commandFiles = fs
 console.log(commandFiles);
 for (const file of commandFiles) {
   const command = require(`../commands/${file}`);
-  commands.push(command.data.toJSON());
+  commands.push(command.getBuilder().toJSON());
 }
 
 const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
@@ -23,13 +23,21 @@ const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
       `Started refreshing ${commands.length} application (/) commands.`
     );
 
-    const data: any = await rest.put(
-      Routes.applicationGuildCommands(
-        process.env.CLIENT_ID,
-        process.env.GUILD_ID
-      ),
-      { body: commands }
-    );
+    let data: Array<SlashCommandBuilder>;
+    if (process.env.GUILD_ID) {
+      data = (await rest.put(
+        Routes.applicationGuildCommands(
+          process.env.CLIENT_ID,
+          process.env.GUILD_ID
+        ),
+        { body: commands }
+      )) as Array<SlashCommandBuilder>;
+    } else {
+      data = (await rest.put(
+        Routes.applicationCommands(process.env.CLIENT_ID),
+        { body: commands }
+      )) as Array<SlashCommandBuilder>;
+    }
 
     console.log(
       `Successfully reloaded ${data.length} application (/) commands.`
